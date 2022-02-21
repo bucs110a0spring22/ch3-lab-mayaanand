b0cf806c7c5fdc488b23734e325dc53c16300901
Maya E E <98714433+mayaanand@users.noreply.github.com>
2022-02-21 01:02:59 +0000

    part a and b

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ michelangelo.color('orange')
 leonardo.color('blue')
 michelangelo.shape('turtle')
 leonardo.shape('turtle')
+leonardo.speed(1)
+michelangelo.speed(1)
 
 michelangelo.up()          # 4.  Pick up the pen so we don’t get lines
 leonardo.up()
@@ -18,9 +20,32 @@ michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 
 ## 5. your code goes here
+michel_random = random.randrange(1,100)
+michelangelo.forward(michel_random)
+print(michel_random)
+leo_random = random.randrange(1,100)
+leonardo.forward(leo_random)
+print(leo_random)
 
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
 
+for i in range(10):
+  distance = random.randrange(1,10)
+  length = random.randrange(1,10)
+  michelangelo.forward(distance)
+  leonardo.forward(length)
+ 
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
 # Part B - complete part B here
+
+
+side = [3, 4, 6, 9, 12] 
+length = 10
+for i in side:
+  angle = (360/side)
+    
 
 
 window.exitonclick()
